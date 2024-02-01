e994fef49d3d3b6236e3684ff25c0a94a68437f3
Gulshan-kumar <121786265+Gulshanray@users.noreply.github.com>
2024-02-01 21:54:11 +0530

    Update database.js

--- database.js
+++ database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/ibm').then(()=>{
+mongoose.connect('mongodb+srv://raygulshan960:sanjukumar@express.ndxrkkr.mongodb.net/?retryWrites=true&w=majority').then(()=>{
     console.log('connect successfully')
 }).catch((error)=>{console.log(error)})
 
