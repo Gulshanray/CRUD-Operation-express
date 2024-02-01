a9acb5e0a756cf8f88ae8d21f8c29453bf1e209b
Gulshan-kumar <121786265+Gulshanray@users.noreply.github.com>
2024-02-01 22:58:22 +0530

    Update database.js

--- database.js
+++ database.js
@@ -1,16 +1,26 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/gulshan').then(()=>{
-    console.log('connect successfully')
-}).catch((error)=>{console.log(error)})
+const connectDB = async () => {
+  try {
+    await mongoose.connect('mongodb://localhost:27017/gulshan', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('Connected to the database successfully');
+  } catch (error) {
+    console.error('Error connecting to the database:', error.message);
+  }
+};
 
-Schema = mongoose.Schema({
-    name:String,
-    mail: String,
-    age:Number
-})
-console.log('schema created')
+connectDB();
 
+const Schema = mongoose.Schema({
+  name: String,
+  mail: String,
+  age: Number,
+});
 
-StudentModel = mongoose.model('student',Schema)
-module.exports =StudentModel
+console.log('Schema created');
+
+const StudentModel = mongoose.model('student', Schema);
+module.exports = StudentModel;
