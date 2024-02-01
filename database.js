57d89d9f5f3979ac7c5838ed44b44581056fafa5
Gulshan-kumar <121786265+Gulshanray@users.noreply.github.com>
2024-02-01 22:13:37 +0530

    Update database.js

--- database.js
+++ database.js
@@ -1,21 +1,16 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://raygulshan960:sanjukumar@express.ndxrkkr.mongodb.net/?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true, serverSelectionTimeoutMS: 30000 })
-    .then(() => {
-        console.log('Connected successfully');
-    })
-    .catch((error) => {
-        console.error('Connection error:', error);
-    });
+mongoose.connect('mongodb://localhost:27017/ibm').then(()=>{
+    console.log('connect successfully')
+}).catch((error)=>{console.log(error)})
 
-const Schema = mongoose.Schema({
-    name: String,
+Schema = mongoose.Schema({
+    name:String,
     mail: String,
-    age: Number
-});
+    age:Number
+})
+console.log('schema created')
 
-console.log('Schema created');
 
-const StudentModel = mongoose.model('student', Schema);
-
-module.exports = StudentModel;
+StudentModel = mongoose.model('student',Schema)
+module.exports =StudentModel
